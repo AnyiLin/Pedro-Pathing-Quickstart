424da8b05c770ad68d11512a35f028109bf0a8af
BaronClaps <126834072+BaronClaps@users.noreply.github.com>
2024-07-04 13:09:24 -0500

    Created OTOS Localizer and got most of the functions done. I just need to create functions to convert OTOS pose2d to Pedro Pose System

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/OTOSLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/OTOSLocalizer.java
@@ -1,4 +1,297 @@
 package org.firstinspires.ftc.teamcode.pedroPathing.localization;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
-public class OTOSLocalizer {
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.MathFunctions;
+import org.firstinspires.ftc.teamcode.pedroPathing.pathGeneration.Vector;
+import org.firstinspires.ftc.teamcode.pedroPathing.util.NanoTimer;
+
+/**
+ * Experimental extension of Localizer that uses the SparkFun OTOS sensor for localization.
+ * <p>
+ * Released under the BSD 3-Clause Clear License by j5155 from 12087 Capital City Dynamics
+ * Portions of this code made and released under the MIT License by SparkFun
+ * Unless otherwise noted, comments are from SparkFun
+ */
+
+public class OTOSLocalizer extends Localizer {
+    private HardwareMap hardwareMap;
+    private Pose startPose;
+    private Pose displacementPose;
+    private Pose currentVelocity;
+    private Matrix prevRotationMatrix;
+    private NanoTimer timer;
+    private long deltaTimeNano;
+    private Pose totalPose;
+    private double totalHeading;
+
+    public static class Params {
+        // Assuming you've mounted your sensor to a robot and it's not centered,
+        // you can specify the offset for the sensor relative to the center of the
+        // robot. The units default to inches and degrees, but if you want to use
+        // different units, specify them before setting the offset! Note that as of
+        // firmware version 1.0, these values will be lost after a power cycle, so
+        // you will need to set them each time you power up the sensor. For example, if
+        // the sensor is mounted 5 inches to the left (negative X) and 10 inches
+        // forward (positive Y) of the center of the robot, and mounted 90 degrees
+        // clockwise (negative rotation) from the robot's orientation, the offset
+        // would be {-5, 10, -90}. These can be any value, even the angle can be
+        // tweaked slightly to compensate for imperfect mounting (eg. 1.3 degrees).
+
+        // these units are inches and radians
+        public SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, Math.toRadians(0));
+
+        // Here we can set the linear and angular scalars, which can compensate for
+        // scaling issues with the sensor measurements. Note that as of firmware
+        // version 1.0, these values will be lost after a power cycle, so you will
+        // need to set them each time you power up the sensor. They can be any value
+        // from 0.872 to 1.127 in increments of 0.001 (0.1%). It is recommended to
+        // first set both scalars to 1.0, then calibrate the angular scalar, then
+        // the linear scalar. To calibrate the angular scalar, spin the robot by
+        // multiple rotations (eg. 10) to get a precise error, then set the scalar
+        // to the inverse of the error. Remember that the angle wraps from -180 to
+        // 180 degrees, so for example, if after 10 rotations counterclockwise
+        // (positive rotation), the sensor reports -15 degrees, the required scalar
+        // would be 3600/3585 = 1.004. To calibrate the linear scalar, move the
+        // robot a known distance and measure the error; do this multiple times at
+        // multiple speeds to get an average, then set the linear scalar to the
+        // inverse of the error. For example, if you move the robot 100 inches and
+        // the sensor reports 103 inches, set the linear scalar to 100/103 = 0.971
+        public double linearScalar = 1.0;
+        public double angularScalar = 1.0;
+    }
+
+    public static OTOSLocalizer.Params PARAMS = new OTOSLocalizer.Params();
+    public SparkFunOTOS otos;
+    private SparkFunOTOS.Pose2D lastOtosPose = otos.getPosition();
+
+    /**
+     * This creates a new ThreeWheelLocalizer from a HardwareMap, with a starting Pose at (0,0)
+     * facing 0 heading.
+     *
+     * @param map the HardwareMap
+     */
+    public OTOSLocalizer(HardwareMap map) {
+        this(map, new Pose());
+    }
+
+    /**
+     * This creates a new ThreeWheelLocalizer from a HardwareMap and a Pose, with the Pose
+     * specifying the starting pose of the localizer.
+     *
+     * @param map the HardwareMap
+     * @param setStartPose the Pose to start from
+     */
+    public OTOSLocalizer(HardwareMap map, Pose setStartPose) {
+
+        hardwareMap = map;
+
+        otos = hardwareMap.get(SparkFunOTOS.class,"sensor_otos");
+
+        otos.setLinearUnit(SparkFunOTOS.LinearUnit.INCHES);
+        otos.setAngularUnit(SparkFunOTOS.AngularUnit.RADIANS);
+
+        otos.setOffset(PARAMS.offset);
+        System.out.println("OTOS calibration beginning!");
+        System.out.println(otos.setLinearScalar(PARAMS.linearScalar));
+        System.out.println(otos.setAngularScalar(PARAMS.angularScalar));
+
+
+        //TODO need to create convert functions
+        otos.setPosition(RRPoseToOTOSPose(startPose));
+        // The IMU on the OTOS includes a gyroscope and accelerometer, which could
+        // have an offset. Note that as of firmware version 1.0, the calibration
+        // will be lost after a power cycle; the OTOS performs a quick calibration
+        // when it powers up, but it is recommended to perform a more thorough
+        // calibration at the start of all your programs. Note that the sensor must
+        // be completely stationary and flat during calibration! When calling
+        // calibrateImu(), you can specify the number of samples to take and whether
+        // to wait until the calibration is complete. If no parameters are provided,
+        // it will take 255 samples and wait until done; each sample takes about
+        // 2.4ms, so about 612ms total
+
+        // RR localizer note: numSamples number completely arbitrary at the moment, feel free to change to fit your needs
+        // Will get better number once I actually get this sensor
+        System.out.println(otos.calibrateImu(255, true));
+        System.out.println("OTOS calibration complete!");
+
+
+        setStartPose(setStartPose);
+        timer = new NanoTimer();
+        deltaTimeNano = 1;
+        displacementPose = new Pose();
+        currentVelocity = new Pose();
+        totalHeading = 0;
+
+        resetOtos();
+    }
+
+    /**
+     * This returns the current pose estimate.
+     *
+     * @return returns the current pose estimate as a Pose
+     */
+    @Override
+    public Pose getPose() {
+        return MathFunctions.addPoses(startPose, displacementPose);
+    }
+
+    /**
+     * This returns the current velocity estimate.
+     *
+     * @return returns the current velocity estimate as a Pose
+     */
+    @Override
+    public Pose getVelocity() {
+        return currentVelocity.copy();
+    }
+
+    /**
+     * This returns the current velocity estimate.
+     *
+     * @return returns the current velocity estimate as a Vector
+     */
+    @Override
+    public Vector getVelocityVector() {
+        return currentVelocity.getVector();
+    }
+
+    /**
+     * This sets the start pose. Changing the start pose should move the robot as if all its
+     * previous movements were displacing it from its new start pose.
+     *
+     * @param setStart the new start pose
+     */
+    @Override
+    public void setStartPose(Pose setStart) {
+        startPose = setStart;
+    }
+
+    /**
+     * This sets the Matrix that contains the previous pose's heading rotation.
+     *
+     * @param heading the rotation of the Matrix
+     */
+    public void setPrevRotationMatrix(double heading) {
+        prevRotationMatrix = new Matrix(3,3);
+        prevRotationMatrix.set(0, 0, Math.cos(heading));
+        prevRotationMatrix.set(0, 1, -Math.sin(heading));
+        prevRotationMatrix.set(1, 0, Math.sin(heading));
+        prevRotationMatrix.set(1, 1, Math.cos(heading));
+        prevRotationMatrix.set(2, 2, 1.0);
+    }
+
+    /**
+     * This sets the current pose estimate. Changing this should just change the robot's current
+     * pose estimate, not anything to do with the start pose.
+     *
+     * @param setPose the new current pose estimate
+     */
+    @Override
+    public void setPose(Pose setPose) {
+        displacementPose = MathFunctions.subtractPoses(setPose, startPose);
+        resetOtos();
+    }
+
+    /**
+     * This updates the elapsed time timer that keeps track of time between updates, as well as the
+     * change position of the Encoders. Then, the robot's global change in position is calculated
+     * using the pose exponential method.
+     */
+    @Override
+    public void update() {
+        deltaTimeNano = timer.getElapsedTime();
+        timer.resetTimer();
+
+        /** Have this disabled, not sure if it auto updates*/
+        //updateEncoders();
+        Matrix robotDeltas = getRobotDeltas();
+        Matrix globalDeltas;
+
+        setPrevRotationMatrix(getPose().getHeading());
+
+        Matrix transformation = new Matrix(3,3);
+        if (Math.abs(robotDeltas.get(2, 0)) < 0.001) {
+            transformation.set(0, 0, 1.0 - (Math.pow(robotDeltas.get(2, 0), 2) / 6.0));
+            transformation.set(0, 1, -robotDeltas.get(2, 0) / 2.0);
+            transformation.set(1, 0, robotDeltas.get(2, 0) / 2.0);
+            transformation.set(1, 1, 1.0 - (Math.pow(robotDeltas.get(2, 0), 2) / 6.0));
+            transformation.set(2, 2, 1.0);
+        } else {
+            transformation.set(0, 0, Math.sin(robotDeltas.get(2, 0)) / robotDeltas.get(2, 0));
+            transformation.set(0, 1, (Math.cos(robotDeltas.get(2, 0)) - 1.0) / robotDeltas.get(2, 0));
+            transformation.set(1, 0, (1.0 - Math.cos(robotDeltas.get(2, 0))) / robotDeltas.get(2, 0));
+            transformation.set(1, 1, Math.sin(robotDeltas.get(2, 0)) / robotDeltas.get(2, 0));
+            transformation.set(2, 2, 1.0);
+        }
+
+        globalDeltas = Matrix.multiply(Matrix.multiply(prevRotationMatrix, transformation), robotDeltas);
+
+        displacementPose.add(new Pose(globalDeltas.get(0, 0), globalDeltas.get(1, 0), globalDeltas.get(2, 0)));
+        currentVelocity = new Pose(globalDeltas.get(0, 0) / (deltaTimeNano * Math.pow(10.0, 9)), globalDeltas.get(1, 0) / (deltaTimeNano * Math.pow(10.0, 9)), globalDeltas.get(2, 0) / (deltaTimeNano * Math.pow(10.0, 9)));
+
+        totalHeading += globalDeltas.get(2, 0);
+    }
+
+
+    /**
+     * This updates the Encoders.
+     * Have this disabled, not sure if it auto updates*/
+
+     /* public void updateEncoders() {
+        otos.update();
+    }*/
+
+    /**
+     * This resets the Encoders.
+     */
+    public void resetOtos() {
+        otos.resetTracking();
+    }
+
+    /**
+     * This calculates the change in position from the perspective of the robot using information
+     * from the Encoders.
+     *
+     * @return returns a Matrix containing the robot relative movement.
+     */
+    public Matrix getRobotDeltas() {
+        Matrix returnMatrix = new Matrix(3,1);
+        // x/forward movement
+        returnMatrix.set(0,0, (PARAMS.linearScalar) * otos.getPosition().x);
+        //y/strafe movement
+        returnMatrix.set(1,0, (PARAMS.linearScalar) * otos.getPosition().y);
+        // theta/turning
+        returnMatrix.set(2,0, (PARAMS.angularScalar) * otos.getPosition().h);
+        return returnMatrix;
+    }
+
+    /**
+     * This returns how far the robot has turned in radians, in a number not clamped between 0 and
+     * 2 * pi radians. This is used for some tuning things and nothing actually within the following.
+     *
+     * @return returns how far the robot has turned in total, in radians.
+     */
+    public double getTotalHeading() {
+        return totalHeading;
+    }
+
+    /**
+     * This is not needed for this sensor but it is here for completeness.
+     *
+     * @return 0 */
+    @Override
+    public double getForwardMultiplier() {
+        return 0;
+    }
+
+    @Override
+    public double getLateralMultiplier() {
+        return 0;
+    }
+
+    @Override
+    public double getTurningMultiplier() {
+        return 0;
+    }
 }
