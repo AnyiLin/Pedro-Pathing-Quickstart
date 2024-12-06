7bc4dbbd341078e25f0499ff31641a00367e540a
junkjunk123 <139065303+junkjunk123@users.noreply.github.com>
2024-12-06 09:18:37 -0800

    Update PoseUpdater.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/PoseUpdater.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/localization/PoseUpdater.java
@@ -351,7 +351,7 @@ public class PoseUpdater {
     /**
      *
      */
-    public void resetIMU() {
+    public void resetIMU() throws InterruptedException {
         localizer.resetIMU();
     }
 }
